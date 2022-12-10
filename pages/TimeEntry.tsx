8dcde982285d30b5e79ffe9480f58b5b8ee8ee5e
Dat Le <letrungcaotung@gmail.com>
2022-12-10 16:30:02 -0400

    Update Time Entry

--- pages/TimeEntry.tsx
+++ pages/TimeEntry.tsx
@@ -1,11 +1,16 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 import Head from 'next/head';
 import stylesTimeEntry from './css/TimeEntry.module.css';
 import Button from '@mui/material/Button';
+import LoadingButton from '@mui/lab/LoadingButton';
 import TextField  from '@mui/material/TextField';
+import SendIcon from '@mui/icons-material/Send';
+import LoginIcon from '@mui/icons-material/Login';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 export default function TimeEntry() {
-  const [dateTime, setdateTime] = useState({ dateTimeArray : [
+  const [loading, setLoading] = useState(false);
+  const [dateTime, setDateTime] = useState({ dateTimeArray : [
                                                               { day: 'Monday', time: 1, },
                                                               { day: 'Tuesday', time: 1, },
                                                               { day: 'Wednesday', time: 1, },
@@ -15,35 +20,59 @@ export default function TimeEntry() {
                                                               { day: 'Sunday', time: 1, },
                                                             ] });
 
+  const [currentDatetime, setCurrentDatetime] = useState(new Date().toLocaleString() + "");
+  const updateCurrentDatetime = useCallback(() => {
+    setCurrentDatetime(() => new Date().toLocaleString() + "");
+  }, [setCurrentDatetime]);
+
+  useEffect(() => {
+    const r = setInterval(() => {
+      updateCurrentDatetime();
+    }, 1000);
+
+    return () => { clearInterval(r) }
+  }, [])
+
   return (
     <>
         <Head>
             <title>Time Entry | {process.env.websiteName}</title>
         </Head>
         <h1>Time Entry</h1>
-        {dateTime.dateTimeArray.map((dateTimeItem) => (
+        <div><i><b>Current Time: </b>{currentDatetime}</i></div>
+        <br/>
+        <div>
+          <Button size="large" variant="outlined" type="submit" color="success" endIcon={<LoginIcon />} className={stylesTimeEntry.ClockInOut}>Clock In</Button>
+          <Button size="large" variant="outlined" type="submit" color="success" endIcon={<LogoutIcon />} className={stylesTimeEntry.ClockInOut}>Clock Out</Button>
+        </div>
+        {dateTime.dateTimeArray.map((dateTimeItem, dateTimeIndex) => (
           <>
-          <div className={stylesTimeEntry.DateDiv}>
+          <div key={dateTimeIndex} className={stylesTimeEntry.DateDiv}>
             <h4 className={stylesTimeEntry.DateLabel}>{dateTimeItem.day}</h4>
             <div className={stylesTimeEntry.TimeEntryDiv}>
-            {[...Array(dateTimeItem.time)].map((timeIndex) => (
+            {[...Array(dateTimeItem.time)].map((timeItem, timeIndex) => (
               <>
               <div key={timeIndex} className={stylesTimeEntry.TimeDiv}>
-                <TextField className={stylesTimeEntry.Input} label="From" variant="standard" type="text" id={"time-from-"+timeIndex} defaultValue={'00:00 AM'}
-                              onClick={event => {if (event.target.defaultValue == '00:00 AM') event.target.defaultValue = '';}}
+                <TextField className={stylesTimeEntry.Input}
+                            label={"From"+(dateTimeItem.time > 1 ? " (" + parseInt(timeIndex+1) + ")": '')}
+                            variant="standard"
+                            type="text"
+                            id={"time-from-"+dateTimeItem.day+"-"+timeIndex}
                 />
               </div>
               <div className={stylesTimeEntry.TimeDiv}>
-              <TextField className={stylesTimeEntry.Input} label="To" variant="standard" type="text" id={"time-to-"+timeIndex} defaultValue={'00:00 AM'}
-                              onClick={event => {if (event.target.defaultValue == '00:00 AM') event.target.defaultValue = '';}}
-                              
+                <TextField className={stylesTimeEntry.Input}
+                            label={"To"+(dateTimeItem.time > 1 ? " (" + parseInt(timeIndex+1) + ")": '')}
+                            variant="standard"
+                            type="text"
+                            id={"time-to-"+dateTimeItem.day+"-"+timeIndex}
                 />
               </div>
               </>
             ))}
             </div>
             <div className={stylesTimeEntry.signDiv}>
-              <i className={`fa-solid fa-minus fa-xl ${stylesTimeEntry.FAIcon}`}
+              <i className={`fa-solid fa-minus fa-xl ${stylesTimeEntry.FAIcon}`} style={{display: (dateTimeItem.time > 1 ? 'block' : 'none')}}
                   onClick={() => {
                         const tmp = [];
                         dateTime.dateTimeArray.map((dateTimeItemTmp) => {
@@ -53,11 +82,11 @@ export default function TimeEntry() {
                           }
                           tmp.push({ day: dateTimeItemTmp.day, time: timeTmp });
                       })
-                        setdateTime({ dateTimeArray: tmp });
+                        setDateTime({ dateTimeArray: tmp });
                       }
                   }
               ></i>
-              <i className={`fa-solid fa-plus fa-xl ${stylesTimeEntry.FAIcon}`}
+              <i className={`fa-solid fa-plus fa-xl ${stylesTimeEntry.FAIcon}`} style={{display: (dateTimeItem.time < 5 ? 'block' : 'none')}}
                   onClick={() => {
                         const tmp = [];
                         dateTime.dateTimeArray.map((dateTimeItemTmp) => {
@@ -67,7 +96,7 @@ export default function TimeEntry() {
                           }
                           tmp.push({ day: dateTimeItemTmp.day, time: timeTmp });
                       })
-                        setdateTime({ dateTimeArray: tmp });
+                        setDateTime({ dateTimeArray: tmp });
                       }
                   }
               ></i>
@@ -75,7 +104,19 @@ export default function TimeEntry() {
           </div>
           </>
         ))}
-        <Button size="large" variant="outlined" type="submit" color="success">Submit</Button>
+        <div>
+          <LoadingButton
+            size="large"
+            onClick={() => {setLoading(true);}}
+            endIcon={<SendIcon />}
+            loading={loading}
+            loadingPosition="end"
+            variant="outlined"
+            color="success"
+          >
+            Submit
+          </LoadingButton>
+        </div>
     </>
   );
 }
